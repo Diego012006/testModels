16267f6cfccc6a5d705226f237e4c3d8b8228984
Diego012006 <diego.delcastillo@utec.edu.pe>
2025-10-20 10:43:39 -0500

    estructura proyecto

---
+++ src/main/java/com/example/testmodels/TestModelsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestModelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestModelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testmodels/TestModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmodels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestModelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
